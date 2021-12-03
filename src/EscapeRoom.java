79ee240b0e373e6bde8a3e79afd312d117f5fcf9
Jay5543 <floydjer000@warren.k12.in.us>
2021-12-03 13:22:28 -0500

    Amend

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,7 +1,73 @@
+import javax.swing.*;
+
 public class EscapeRoom {
 
+
+    /*
+    Plan - Escape Room
+    Make a GUI
+    Make searios
+     */
+
+    // J-Frame
+        static JFrame EscapeRoomGameWindow;
+    // J-Panel
+        static JPanel EscapeRoomGamePanel;
+    // J-button
+        //Bed Buttons
+        static JButton bedButtonThatBringsYouToTheBed;
+        static JButton bedButtonThatMakesYouLookUnderTheBed;
+        static JButton bedButtonThatMakesYouMoveTheCover;
+        static JButton pickUpThePhoneFromTheBed;
+
+        //Door Buttons
+        static JButton doorKnob;
+        static JButton goToTheDoor;
+
+        //Pass Code
+    static JButton topLeftNumber1
+    static JButton topMiddleNumber2
+    static JButton topRightNumber
+    static JButton
+    static JButton
+    static JButton
+    static JButton
+    static JButton
+    static JButton
+    // J-Labels
+
+
+    // J-Text Field
     public static void main(String[] args) {
 
+        //Main Methods
+        gui();
     }
+    //GUI
+    public static void gui(){
+
+        //Window
+        EscapeRoomGameWindow = new JFrame("ESCAPE ROOM");
+        EscapeRoomGameWindow.setSize(1350, 750);
+        EscapeRoomGameWindow.setLocationRelativeTo(null);
+        EscapeRoomGameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Panel
+        EscapeRoomGamePanel = new JPanel();
+        //Buttons
+
+        //Labels
+
+        //.setVisable
+        EscapeRoomGameWindow.setVisible(true);
+        EscapeRoomGamePanel.setVisible(true);
+
+    }
+
+    //Bed
+
+    //Door
+
+    //Pass Code
 
 }
